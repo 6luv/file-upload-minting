8ea6aadcefc58beace8ec127ec5ed09ed6978271
6luv <jmj00005@naver.com>
2024-06-17 09:46:22 +0900

    feat: signer, contract

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { FC, useEffect, useState } from "react";
+import { Contract, JsonRpcSigner, ethers } from "ethers";
+import mintContractAbi from "./mintContractAbi.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: FC = () => {
+  const [signer, setSigner] = useState<JsonRpcSigner | null>(null);
+  const [mintContract, setMintContract] = useState<Contract | null>(null);
+
+  const onClickMetamaask = async () => {
+    try {
+      if (!window.ethereum) return;
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      setSigner(await provider.getSigner());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!signer) return;
+
+    setMintContract(
+      new Contract(
+        "0xABed627BABa39ce1b6a6B56b6257De476c5055e5",
+        mintContractAbi,
+        signer
+      )
+    );
+  }, [signer]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Flex
+      bgColor="red.100"
+      w="100%"
+      minH="100vh"
+      justifyContent="center"
+      alignItems="center"
+      flexDir="column"
+    >
+      {signer ? (
+        <Text>{signer.address}</Text>
+      ) : (
+        <Button onClick={onClickMetamaask}>로그인</Button>
+      )}
+    </Flex>
+  );
+};
 
-export default App
+export default App;
